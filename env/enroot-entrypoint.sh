0aca0d794ad9b61e2303de8b4476f51399ecad1a
Lukas Drescher <lukas.drescher@cscs.ch>
2024-02-21 17:42:03 +0100

    Fix enroot entrypoint

--- env/enroot-entrypoint.sh
+++ env/enroot-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-ln -s /bin/bash /usr/local/bin/bash
 
 mpi_local_rank=${SLURM_LOCALID:-0}
 enroot_ssh_port=${ENROOT_SSH_PORT:-15263}
 
 if [ "${mpi_local_rank}" -eq 0 ]; then
+    ln -s /bin/bash /usr/local/bin/bash
     echo "[enroot-entrypoint] Launching SSH-server on $(hostname) at port ${enroot_ssh_port}"
     sed -i -e "s/#Port 22/Port ${enroot_ssh_port}/g" /etc/ssh/sshd_config
     /usr/sbin/sshd
